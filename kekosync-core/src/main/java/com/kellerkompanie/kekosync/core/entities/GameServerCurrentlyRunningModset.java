b166418f39a9c924258a214455e15c73c58f3285
destotelhorus <dth@dthweb.net>
2018-04-02 01:00:48 +0200

    Added entity for currently running modset.

---
+++ kekosync-core/src/main/java/com/kellerkompanie/kekosync/core/entities/GameServerCurrentlyRunningModset.java
@@ -0,0 +1,42 @@
+package com.kellerkompanie.kekosync.core.entities;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author dth
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@EqualsAndHashCode
+public class GameServerCurrentlyRunningModset implements Serializable {
+    @Getter private String name;
+    @Getter private UUID uuid;
+    @Getter private String address;
+    @Getter private String port;
+
+    private List<Mod> requiredMods = new ArrayList<>();
+    private List<Mod> optionalMods = new ArrayList<>();
+
+    public List<Mod> getRequiredMods() {
+        return Collections.unmodifiableList(requiredMods);
+    }
+    public List<Mod> getOptionalMods() {
+        return Collections.unmodifiableList(optionalMods);
+    }
+
+    public void addRequiredMod(Mod mod) {
+        requiredMods.add(mod);
+    }
+    public void removeRequiredMod(Mod mod) {
+        requiredMods.remove(mod);
+    }
+
+    public void addOptionalMod(Mod mod) { optionalMods.add(mod); }
+    public void removeOptionalMod(Mod mod) { optionalMods.remove(mod); }
+}
