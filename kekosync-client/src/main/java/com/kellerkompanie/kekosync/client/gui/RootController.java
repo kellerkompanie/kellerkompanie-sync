985d6f10d7ff8e94e27e32f47cfc833f320f52c2
Schwaggot <tom.ryan@posteo.de>
2018-04-02 13:08:25 +0200

    added persistency to gui window size, position and maximized state

--- kekosync-client/src/main/java/com/kellerkompanie/kekosync/client/gui/RootController.java
+++ kekosync-client/src/main/java/com/kellerkompanie/kekosync/client/gui/RootController.java
@@ -1,6 +1,10 @@
 package com.kellerkompanie.kekosync.client.gui;
 
+import com.kellerkompanie.kekosync.client.settings.Settings;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,6 +14,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Set;
 
 public class RootController extends Application {
     @FXML
@@ -18,18 +23,70 @@ public class RootController extends Application {
     @FXML
     private Tab settingTab;
 
+    private Scene scene;
+    private Stage stage;
+
     public static void openWindow(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
+        stage = primaryStage;
         primaryStage.setTitle("KekoSync");
 
-        Parent root = loadFXML("root");
+        double width = Settings.getInstance().getWindowWidth();
+        double height = Settings.getInstance().getWindowHeight();
+        double x = Settings.getInstance().getWindowX();
+        double y = Settings.getInstance().getWindowY();
+        boolean maximized = Settings.getInstance().isWindowMaximized();
 
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMaximized(maximized);
+
+        primaryStage.setX(x);
+        primaryStage.setY(y);
+
+        Parent root = loadFXML("root");
+        scene = new Scene(root, width, height);
+
+        scene.widthProperty().addListener(this::onWindowSizeChanged);
+        scene.heightProperty().addListener(this::onWindowSizeChanged);
+
+        primaryStage.setScene(scene);
+
         primaryStage.show();
+
+        stage.xProperty().addListener(this::onWindowPositionChanged);
+        stage.yProperty().addListener(this::onWindowPositionChanged);
+        stage.maximizedProperty().addListener(this::onWindowMaximizedChanged);
+    }
+
+    private static final int SAVE_THRESHOLD = 5000;
+    private long timestmapLastWindowMovement = -1;
+    private long timestampLastWindowResize = -1;
+
+    private void onWindowPositionChanged(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+        double x = stage.getX();
+        double y = stage.getY();
+
+        long currentTimestamp = System.currentTimeMillis();
+        if(timestmapLastWindowMovement != -1 && (currentTimestamp - timestmapLastWindowMovement) > SAVE_THRESHOLD) {
+            Settings.getInstance().updateWindowPosition(x, y);
+        }
+    }
+
+    private void onWindowSizeChanged(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+        double width = scene.getWidth();
+        double height = scene.getHeight();
+
+        long currentTimestamp = System.currentTimeMillis();
+        if(timestampLastWindowResize != -1 && (currentTimestamp - timestampLastWindowResize) > SAVE_THRESHOLD) {
+            Settings.getInstance().updateWindowSize(width, height);
+        }
+    }
+
+    private void onWindowMaximizedChanged(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+        Settings.getInstance().updateWindowMaximized(newValue);
     }
 
     private Parent loadFXML(String name) {
