1bb3f367580d87e2439d2ea0daae06ea07a93d84
Schwaggot <tom.ryan@posteo.de>
2018-04-02 16:06:04 +0200

    removed obsolete import

--- kekosync-client/src/main/java/com/kellerkompanie/kekosync/client/gui/LauncherOptionsController.java
+++ kekosync-client/src/main/java/com/kellerkompanie/kekosync/client/gui/LauncherOptionsController.java
@@ -1,6 +1,5 @@
 package com.kellerkompanie.kekosync.client.gui;
 
-import com.kellerkompanie.kekosync.client.arma.ArmALauncher;
 import com.kellerkompanie.kekosync.client.arma.ArmAParameter;
 import com.kellerkompanie.kekosync.client.settings.Settings;
 import javafx.event.ActionEvent;
@@ -13,8 +12,13 @@ import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ResourceBundle;
 
+/**
+ * @author Schwaggot
+ */
 public class LauncherOptionsController implements Initializable {
 
     @FXML
@@ -27,13 +31,13 @@ public class LauncherOptionsController implements Initializable {
         HashMap<String, ArmAParameter> params = Settings.getInstance().getLaunchParams();
 
         List<Node> children = paramVBox.getChildren();
-        for(Node child : children) {
+        for (Node child : children) {
             ArmAParameter param = params.get(child.getId());
-            if(param != null) {
-                if(child instanceof CheckBox) {
+            if (param != null) {
+                if (child instanceof CheckBox) {
                     CheckBox cb = (CheckBox) child;
                     cb.setSelected(param.isEnabled());
-                } else if(child instanceof ComboBox) {
+                } else if (child instanceof ComboBox) {
                     ComboBox cb = (ComboBox) child;
                     cb.getSelectionModel().select(param.getValue());
                 }
