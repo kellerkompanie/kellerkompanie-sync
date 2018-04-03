e167177d6650efd88965d1e6f846336598514774
Schwaggot <tom.ryan@posteo.de>
2018-04-03 14:17:59 +0200

    added jetty server to host debug repo

---
+++ kekosync-hoster/src/main/java/com/kellerkompanie/kekosync/hoster/KekoSyncHoster.java
@@ -0,0 +1,42 @@
+package com.kellerkompanie.kekosync.hoster;
+
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
+public class KekoSyncHoster {
+    public static void main(String[] args) throws Exception {
+        // Create a basic Jetty server object that will listen on port 8080.  Note that if you set this to port 0
+        // then a randomly available port will be assigned that you can either look in the logs for the port,
+        // or programmatically obtain it for use in test cases.
+        Server server = new Server(80);
+
+        // Create the ResourceHandler. It is the object that will actually handle the request for a given file. It is
+        // a Jetty Handler object so it is suitable for chaining with other handlers as you will see in other examples.
+        ResourceHandler resource_handler = new ResourceHandler();
+        // Configure the ResourceHandler. Setting the resource base indicates where the files should be served out of.
+        // In this example it is the current directory but it can be configured to anything that the jvm has access to.
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setWelcomeFiles(new String[]{"index.html"});
+        resource_handler.setResourceBase("E:/kekosync-demo-repository/");
+
+        ContextHandler context = new ContextHandler();
+        context.setContextPath( "/repo" );
+        context.setHandler( resource_handler );
+
+        server.setHandler( context );
+
+        // Add the ResourceHandler to the server.
+        //HandlerList handlers = new HandlerList();
+        //handlers.setHandlers(new Handler[]{resource_handler, new DefaultHandler()});
+        //server.setHandler(handlers);
+
+        // Start things up! By using the server.join() the server thread will join with the current thread.
+        // See "http://docs.oracle.com/javase/1.5.0/docs/api/java/lang/Thread.html#join()" for more details.
+        server.start();
+        server.join();
+    }
+}
