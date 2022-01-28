9833302435ebdd4f5f543ae032f956858f58a05f
Schwaggot <tom.ryan@posteo.de>
2022-01-28 21:01:37 +0100

    Uploading status before formatting disk

--- kekosync-client/src/main/java/com/kellerkompanie/kekosync/client/ExampleRunthroughRunner.java
+++ kekosync-client/src/main/java/com/kellerkompanie/kekosync/client/ExampleRunthroughRunner.java
@@ -18,42 +18,42 @@ import static com.kellerkompanie.kekosync.core.helper.FileSyncHelper.*;
 public class ExampleRunthroughRunner {
 
     public static void main(String[] args) throws Exception {
-        final String repoBaseURL =  "http://localhost/repo/";
-
-        // Getting the modgroups
-        String repoJsonString = HttpHelper.readUrl(repoBaseURL + Filenames.FILENAME_MODGROUPS);
-        Repository repository = new Gson().fromJson(repoJsonString, Repository.class);
-
-        log.info("{}", repository);
-
-        String indexJsonString = HttpHelper.readUrl(repoBaseURL + Filenames.FILENAME_INDEXFILE);
-        FileindexEntry rootindexEntry = new Gson().fromJson(indexJsonString, FileindexEntry.class);
-
-        log.info("{}", "local results");
-        log.info("{}", rootindexEntry);
-
-        ModGroup allModGroup = repository.getModGroups().get(0);
-
-        ModGroup onlyAceGroup = new ModGroup("onlyacegroup", UUID.randomUUID(), new HashSet<>());
-        ModGroup moreAceGroup = new ModGroup("moreacegroup", UUID.randomUUID(), new HashSet<>());
-
-        for (Mod mod: allModGroup.getMods()) {
-//            if ( mod.getName().equals("@ace") ) onlyAceGroup.addMod(mod);
-//            if ( mod.getName().equals("@ace") ) moreAceGroup.addMod(mod);
-            if ( mod.getName().equals("@acex") ) moreAceGroup.addMod(mod);
-        }
-
-        FileindexEntry limitedFileindexEntry = limitFileindexToModgroups(rootindexEntry, allModGroup);
-
-//        syncFileindexTree(limitedFileindexEntry, Paths.get("E:\\kekosync-local"), repoBaseURL);
-        FileindexWithSyncEntry fileindexWithSyncEntry = checksyncFileindexTree(limitedFileindexEntry, Paths.get("E:\\kekosync-local"));
-
-
-        log.info("{}", "done");
-
-
-        //Zsync zsync = new Zsync();
-        //URI zsyncFileBaseURL = URI.create(repoBaseURL + "/@ace/ace_advanced_ballistics.dll.zync");
-        //zsync.zsync(zsyncFileBaseURL);
+//        final String repoBaseURL =  "http://localhost/repo/";
+//
+//        // Getting the modgroups
+//        String repoJsonString = HttpHelper.readUrl(repoBaseURL + Filenames.FILENAME_MODGROUPS);
+//        Repository repository = new Gson().fromJson(repoJsonString, Repository.class);
+//
+//        log.info("{}", repository);
+//
+//        String indexJsonString = HttpHelper.readUrl(repoBaseURL + Filenames.FILENAME_INDEXFILE);
+//        FileindexEntry rootindexEntry = new Gson().fromJson(indexJsonString, FileindexEntry.class);
+//
+//        log.info("{}", "local results");
+//        log.info("{}", rootindexEntry);
+//
+//        ModGroup allModGroup = repository.getModGroups().get(0);
+//
+//        ModGroup onlyAceGroup = new ModGroup("onlyacegroup", UUID.randomUUID(), new HashSet<>());
+//        ModGroup moreAceGroup = new ModGroup("moreacegroup", UUID.randomUUID(), new HashSet<>());
+//
+//        for (Mod mod: allModGroup.getMods()) {
+////            if ( mod.getName().equals("@ace") ) onlyAceGroup.addMod(mod);
+////            if ( mod.getName().equals("@ace") ) moreAceGroup.addMod(mod);
+//            if ( mod.getName().equals("@acex") ) moreAceGroup.addMod(mod);
+//        }
+//
+//        FileindexEntry limitedFileindexEntry = limitFileindexToModgroups(rootindexEntry, allModGroup);
+//
+////        syncFileindexTree(limitedFileindexEntry, Paths.get("E:\\kekosync-local"), repoBaseURL);
+//        FileindexWithSyncEntry fileindexWithSyncEntry = checksyncFileindexTree(limitedFileindexEntry, Paths.get("E:\\kekosync-local"));
+//
+//
+//        log.info("{}", "done");
+//
+//
+//        //Zsync zsync = new Zsync();
+//        //URI zsyncFileBaseURL = URI.create(repoBaseURL + "/@ace/ace_advanced_ballistics.dll.zync");
+//        //zsync.zsync(zsyncFileBaseURL);
     }
 }
